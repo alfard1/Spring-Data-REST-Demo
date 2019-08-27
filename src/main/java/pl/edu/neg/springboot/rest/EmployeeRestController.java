f606e0843c798f4cdbbd5a60109002720e33b69a
Pawel Wysokinski <pawel.wysokinski@gmail.com>
2019-08-27 22:35:37 +0200

    added all CRUD operations for REST API with Hibernate

--- src/main/java/pl/edu/neg/springboot/rest/EmployeeRestController.java
+++ src/main/java/pl/edu/neg/springboot/rest/EmployeeRestController.java
@@ -1,9 +1,7 @@
 package pl.edu.neg.springboot.rest;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import pl.edu.neg.springboot.entity.Employee;
 import pl.edu.neg.springboot.service.EmployeeService;
 
@@ -25,4 +23,51 @@ public class EmployeeRestController {
     public List<Employee> findAll() {
         return employeeService.findAll();
     }
+
+    // mapping for GET /employees/{employeeId}
+    @GetMapping("/employees/{employeeId}")
+    public Employee getEmployee(@PathVariable int employeeId) {
+
+        Employee theEmployee = employeeService.findById(employeeId);
+
+        if (theEmployee == null) {
+            throw new RuntimeException("Employee with id = " + employeeId + " not found.");
+        }
+
+        return theEmployee;
+    }
+
+    // mapping for POST /employees - add new employee
+    @PostMapping("/employees")
+    public Employee addEmployee(@RequestBody Employee theEmployee) {
+
+        // set id to 0 to force a save of new item, instead of update
+        theEmployee.setId(0);
+
+        employeeService.save(theEmployee);
+        return theEmployee;
+    }
+
+    // mapping for PUT /employees - update employee
+    @PutMapping("/employees")
+    public Employee updateEmployee(@RequestBody Employee theEmployee) {
+        employeeService.save(theEmployee);
+        return theEmployee;
+    }
+
+    // mapping for delete /employees/{employeeId} - delete employee
+    @DeleteMapping("/employees/{employeeId}")
+    public String deleteEmployee(@PathVariable int employeeId) {
+
+        Employee tempEmployee = employeeService.findById(employeeId);
+
+        if (tempEmployee == null) {
+            throw new RuntimeException("Employee with id = " + employeeId + " doesn't exist.");
+        }
+
+        employeeService.deleteById(employeeId);
+        return "Employee with id = " + employeeId + " deleted.";
+    }
+
+
 }
